1f719429d363dec0102ed84742dedebb5eae42fc
ZackZhao556 <zackzhao2020@163.com>
2022-11-27 14:00:47 -0600

    The operation logic functions

---
+++ logic.py
@@ -0,0 +1,146 @@
+import random
+import constants as c
+
+### Create a new n*n 2-d list (an empty matrix to initialize the game,we can use 0 or None to impute the matrix)
+def new_game(n):
+    matrix = []
+    for i in range(n):
+        matrix.append([0] * n)
+    matrix = add_two(matrix)
+    matrix = add_two(matrix)
+    return matrix
+
+### Randomly choose a cell and assign 2 to its value
+
+def add_two(mat):
+    a = random.randint(0, len(mat)-1)
+    b = random.randint(0, len(mat)-1)
+    while mat[a][b] != 0:
+        a = random.randint(0, len(mat)-1)
+        b = random.randint(0, len(mat)-1)
+    mat[a][b] = 2
+    return mat
+
+### Use for loops to check game status for each situation:
+##### 1. If there is a cell reach 2048, player wins
+##### 2. If there every single cell is not 0 and there is no same value for the neighbor cell, then lose
+##### 3. Otherwise, it is not over
+
+def game_state(mat):
+    # check for 2048
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            if mat[i][j] == 2048:
+                return 'win'
+    # check for empty cell
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            if mat[i][j] == 0:
+                return 'not over'
+    # check for same cells that touch each other
+    for i in range(len(mat)-1):
+        for j in range(len(mat[0])-1):
+            if mat[i][j] == mat[i+1][j] or mat[i][j+1] == mat[i][j]:
+                return 'not over'
+    # To avoid the index out of the range, we have to examine the last row horizontally and last column vertically
+    for k in range(len(mat)-1):  # to check the left/right entries on the last row
+        if mat[len(mat)-1][k] == mat[len(mat)-1][k+1]:
+            return 'not over'
+    for j in range(len(mat)-1):  # check up/down entries on last column
+        if mat[j][len(mat)-1] == mat[j+1][len(mat)-1]:
+            return 'not over'
+    return 'lose'
+
+### Due to the whole playing process, we can get the "post-operation" matrix by reverse or transpose or reverse plus transpose
+#### 1. reverse means: [0,0,2,8] ---> [8,2,0,0] which is the operation of 'left'.
+#### 2. Transpose means:[[0,1],[2,3]] ---> [[0,2],[1,3]]
+
+def reverse(mat):
+    new = []
+    for i in range(len(mat)):
+        new.append([])
+        for j in range(len(mat[0])):
+            new[i].append(mat[i][len(mat[0])-j-1])
+    return new
+
+def transpose(mat):
+    new = []
+    for i in range(len(mat[0])):
+        new.append([])
+        for j in range(len(mat)):
+            new[i].append(mat[j][i])
+    return new
+
+
+### Get the matrix compressed, which means get all the non-zero value stay close to each other, and left the zeros
+### after. Ex: [2,4,0,0,8] --->[2,4,8,0,0]. For the realization part, I read the code from yangshun repo and managed to
+### understand the logic behind the code. The main idea behind the cover_up funtion is based on whichever the opertaion
+### is, the zeros will shift to the end of the direction of operation. And if there are two cell next to each other
+### in the covered-up matrix has the same value, we should add them up. And let the latter one becomes zero. So the whole
+### proces of a single operation will be: we covered up the matrix, do the merge function, and we do the cover-up again.
+### the reason we cover up again is because after merge, we assign the latter cell to 0, there is a chance that 0 is in
+### the middle of list.
+def cover_up(mat):
+    new = []
+    for j in range(c.GRID_LEN):
+        partial_new = []
+        for i in range(c.GRID_LEN):
+            partial_new.append(0)
+        new.append(partial_new)
+    done = False
+    for i in range(c.GRID_LEN):
+        count = 0
+        for j in range(c.GRID_LEN):
+            if mat[i][j] != 0:
+                new[i][count] = mat[i][j]
+                if j != count:
+                    done = True
+                count += 1
+    return new, done
+
+def merge(mat, done):
+    for i in range(c.GRID_LEN):
+        for j in range(c.GRID_LEN-1):
+            if mat[i][j] == mat[i][j+1] and mat[i][j] != 0:
+                mat[i][j] *= 2
+                mat[i][j+1] = 0
+                done = True
+    return mat, done
+
+def up(game):
+    print("up")
+    # return matrix after shifting up
+    game = transpose(game)
+    game, done = cover_up(game)
+    game, done = merge(game, done)
+    game = cover_up(game)[0]
+    game = transpose(game)
+    return game, done
+
+def down(game):
+    print("down")
+    # return matrix after shifting down
+    game = reverse(transpose(game))
+    game, done = cover_up(game)
+    game, done = merge(game, done)
+    game = cover_up(game)[0]
+    game = transpose(reverse(game))
+    return game, done
+
+def left(game):
+    print("left")
+    # return matrix after shifting left
+    game, done = cover_up(game)
+    game, done = merge(game, done)
+    game = cover_up(game)[0]
+    return game, done
+
+def right(game):
+    print("right")
+    # return matrix after shifting right
+    game = reverse(game)
+    game, done = cover_up(game)
+    game, done = merge(game, done)
+    game = cover_up(game)[0]
+    game = reverse(game)
+    return game, done
